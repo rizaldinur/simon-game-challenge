7c85fce18ae7a611fe2ebc433276e9acc58aa9c5
rizaldinur <rnur71@gmail.com>
2024-05-15 14:03:25 +0700

    add animation and play sound for color selected

--- game.js
+++ game.js
@@ -5,4 +5,16 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
+  //Step 3
+
+  //1. select button with id == randomChosenColour
+  //2. make flashing animation
+  $("#" + randomChosenColour)
+    .fadeOut(200)
+    .fadeIn(100);
+
+  //3 play sound for button color selected
+  var sound = new Audio("./sounds/" + randomChosenColour + ".mp3");
+  sound.play();
 }
