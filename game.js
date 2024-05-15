92251cbd9a1c8bab2f9eb8aed5d054a33efc64a9
rizaldinur <rnur71@gmail.com>
2024-05-15 13:46:39 +0700

    create sequence pattern

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
-var randomNumber = Math.floor(Math.random() * 4);
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+}
