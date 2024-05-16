43113d7b38f7d54bcdb5e97cd6766521dc324de5
rizaldinur <rnur71@gmail.com>
2024-05-16 12:52:35 +0700

    add logic , gameover, restart
    check answer based on user click and gamepattern.
    Add gameover function when wrong click. Gameover sound, header, then restart game.

--- game.js
+++ game.js
@@ -1,6 +1,84 @@
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 var userClickedPattern = [];
+var level = 0;
+
+//at Start game
+//Press keyboard only once to start game
+$(document).ready(function () {
+  if (level === 0) {
+    $(this).on("keydown", function (e) {
+      nextSequence();
+    });
+  }
+});
+
+//Step 4
+//1 create click event handler
+var answer = false;
+var clickCount = 0;
+$(".btn").on("click", function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  //itung setiap click
+  clickCount++;
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  //cek jawaban berdasarkan tombol warna yg diklik
+  //jika klik terakhir benar, lanjut sampai klik = length gamepattern
+  answer = checkAnswer(clickCount);
+  if (answer === true) {
+    //tunggu sampai klik = panjang/jumlah gamepattern
+    if (clickCount === gamePattern.length) {
+      setTimeout(() => {
+        //resset click pattern dan click count
+        userClickedPattern.length = 0;
+        clickCount = 0;
+
+        //next
+        nextSequence();
+      }, 1000);
+    }
+    return;
+  }
+  //jika salah, gameover
+  else {
+    gameOver();
+  }
+});
+
+function gameOver() {
+  //reset level, reset gamepattern, clickpattern, clickcount
+  level = 0;
+  userClickedPattern.length = 0;
+  gamePattern.length = 0;
+  clickCount = 0;
+
+  //header ganti
+  $("h1").text("Game Over, Press Any Key to Restart");
+
+  //animasikan background flashing merah
+  $("body").addClass("game-over");
+
+  setTimeout(function () {
+    $("body").removeClass("game-over");
+  }, 100);
+
+  //play sound game over
+  playSound("wrong");
+}
+
+//step 8 check answer on every last color that user click button
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel - 1] === userClickedPattern[currentLevel - 1]) {
+    console.log("correct");
+    return true;
+  }
+  console.log("wrong");
+  return false;
+}
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
@@ -17,46 +95,11 @@ function nextSequence() {
 
   //3 play sound for button color selected
   playSound(randomChosenColour);
+
+  //change level every new sequence
+  level++;
+  $("h1").text("Level " + level);
 }
-
-//Step 4
-//1 create click event handler
-
-$("#green").on("click", function () {
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-  console.log(userChosenColour);
-});
-
-$("#yellow").on("click", function () {
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-  console.log(userChosenColour);
-});
-
-$("#red").on("click", function () {
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-  console.log(userChosenColour);
-});
-
-$("#blue").on("click", function () {
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-  console.log(userChosenColour);
-});
 
 function playSound(name) {
   var sound = new Audio("./sounds/" + name + ".mp3");
